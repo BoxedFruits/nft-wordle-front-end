435d1efeb61488dd704a1826b6bb37328bcc490e
boxedfruits <samballesteros98@gmail.com>
2022-02-21 22:44:45 -0500

    implement onboarding button

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,23 +1,85 @@
-import React from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import { useDispatch } from 'react-redux';
 import { useAppSelector } from '../app/hooks';
 import { AppDispatch } from '../configureStore';
 import { updateAddress } from '../ducks/address';
+import MetaMaskOnboarding from '@metamask/onboarding';
+import './header.css';
+
+const ONBOARD_TEXT = 'Click here to install MetaMask!';
+const CONNECT_TEXT = 'Connect';
+
+function OnboardingButton() {
+  const [buttonText, setButtonText] = useState(ONBOARD_TEXT);
+  const [isDisabled, setDisabled] = useState(false);
+  const [accounts, setAccounts] = useState([]);
+  const onboarding = useRef<MetaMaskOnboarding>();
+  const dispatch: AppDispatch = useDispatch();
+
+  useEffect(() => {
+    if (!onboarding.current) {
+      onboarding.current = new MetaMaskOnboarding();
+    }
+  }, []);
+
+  useEffect(() => {
+    if (MetaMaskOnboarding.isMetaMaskInstalled()) {
+      if (accounts.length > 0) {
+        setButtonText(accounts[0]);
+        setDisabled(true);
+        onboarding.current.stopOnboarding();
+        dispatch({...updateAddress(accounts[0])});
+      } else {
+        setButtonText(CONNECT_TEXT);
+        setDisabled(false);
+      }
+    }
+  }, [accounts]);
+
+  useEffect(() => {
+    function handleNewAccounts(newAccounts) {
+      setAccounts(newAccounts);
+    }
+    if (MetaMaskOnboarding.isMetaMaskInstalled()) {
+      window.ethereum
+        .request({ method: 'eth_requestAccounts' })
+        .then(handleNewAccounts);
+      window.ethereum.on('accountsChanged', handleNewAccounts);
+      return () => {
+        window.ethereum.off('accountsChanged', handleNewAccounts);
+      };
+    }
+  }, []);
+
+  const onClick = () => {
+    if (MetaMaskOnboarding.isMetaMaskInstalled()) {
+      window.ethereum
+        .request({ method: 'eth_requestAccounts' })
+        .then((newAccounts) => setAccounts(newAccounts));
+    } else {
+      onboarding.current.startOnboarding();
+    }
+  };
+  return (
+    <button className="connect-metamask" disabled={isDisabled} onClick={onClick}>
+      {buttonText}
+    </button>
+  );
+}
 
 
 //Need to add web3 stuff
+//ABI
+//ContractAddress
 function Header() {
-  const dispatch: AppDispatch = useDispatch();
-  function clicked() {
-    dispatch({...updateAddress("my New address")});
-  }
+
+
   const state = useAppSelector(state => state.addressReducer.address);
-  console.log(state);
 
   return (<div>
     This is my header
-    <button onClick={clicked}> Connect </button>
     <button onClick={() => {console.log(state)}}> get state</button>
+    <OnboardingButton></OnboardingButton>
   </div>);
 }
 
