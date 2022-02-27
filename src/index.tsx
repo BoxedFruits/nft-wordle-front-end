128e260cc38ce334b5e6620b3a08c814cbe56e0c
boxedfruits <samballesteros98@gmail.com>
2022-02-26 20:31:26 -0500

    update import

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './app/App';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
 import store from '../src/configureStore'
 import { Provider } from 'react-redux'
