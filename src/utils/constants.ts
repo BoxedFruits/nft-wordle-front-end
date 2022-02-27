7a4518965da9dc78514f41113c70ef6b9cbc8092
boxedfruits <samballesteros98@gmail.com>
2022-02-27 18:04:10 -0500

    update contract address

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,6 +1,6 @@
-export const contractAddress ="0xB6Adc3EFDb2bdcDdb14EecD757DF265Bff03cf0C";
+export const contractAddress ="0xd51dED6Bb2997D7eB5e39dFA48FE6fbF78840142";
 
-export const MAX_WORD_LENGTH = 5
+export const MAX_WORD_LENGTH = 6
 export const MAX_CHALLENGES = 6
 export const ALERT_TIME_MS = 2000
 export const REVEAL_TIME_MS = 350
